2a07a192f66f07798e474d7109f6c571f2fb301f
Andersonhere <1694064386@qq.com>
2021-05-23 03:47:57 -0700

    local test

--- pub_all.sh
+++ pub_all.sh
@@ -1,4 +1,7 @@
 git add . & git commit -m "$1" & git push origin hexo_all
 
+
+hexo new post "new bolg"
+
 hexo g 
 hexo d
---
+++ source/_posts/local-test.md
@@ -0,0 +1,5 @@
+---
+title: local test
+date: 2021-05-23 03:47:18
+tags:
+---
