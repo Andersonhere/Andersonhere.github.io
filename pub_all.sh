ebaacb727b89d79ef132cc327dd7e00e30550167
gaoqi <gaoqi@qianxin.com>
2021-06-21 20:23:37 +0800

    install step

--- pub_all.sh
+++ pub_all.sh
@@ -1,3 +1,14 @@
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+
+sudo apt-get install -y nodejs
+
+sudo npm install hexo-cli g
+
+
+
+
+
+
 git add . & git commit -m "$1" & git push origin hexo_all
 
 
